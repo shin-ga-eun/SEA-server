d7d4188db64b29385ba5bf8355b0e3e7f6ff867f
shin-ga-eun <sge7102@naver.com>
2020-05-25 22:19:31 +0900

    #1 dir 수정.

---
+++ src/main/java/com/bugkillers/domain/entity/User.java
@@ -0,0 +1,96 @@
+package com.bugkillers.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private long uid;
+    private String platform_type;
+    private String access_token;
+    private String email;
+    private String password;
+    private String role;
+    private String name;
+    private String image;
+    private String profile;
+
+    public long getUid() {
+        return uid;
+    }
+
+    public void setUid(long uid) {
+        this.uid = uid;
+    }
+
+    public String getPlatform_type() {
+        return platform_type;
+    }
+
+    public void setPlatform_type(String platform_type) {
+        this.platform_type = platform_type;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+}
