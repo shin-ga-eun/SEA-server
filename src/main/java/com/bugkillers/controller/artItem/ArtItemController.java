8ec30fbcb3605ef70fce9f536b0bf0f8a2ebbabe
shin-ga-eun <sge7102@naver.com>
2020-06-15 02:23:52 +0900

    #4 작품수정, 삭제, ano로 조회구현.

--- src/main/java/com/bugkillers/controller/artItem/ArtItemController.java
+++ src/main/java/com/bugkillers/controller/artItem/ArtItemController.java
@@ -1,11 +1,12 @@
 package com.bugkillers.controller.artItem;
 
+import com.bugkillers.domain.dto.artItem.GetArtItemDto;
 import com.bugkillers.domain.dto.artItem.SaveArtItemDto;
-import com.bugkillers.domain.dto.member.LoginDto;
-import com.bugkillers.domain.dto.member.SignUpDto;
+import com.bugkillers.domain.dto.artItem.UpdateArtItemDto;
+import com.bugkillers.service.artItem.DeleteArtItemService;
+import com.bugkillers.service.artItem.GetArtItemService;
 import com.bugkillers.service.artItem.SaveArtItemService;
-import com.bugkillers.service.member.LoginService;
-import com.bugkillers.service.member.SignUpService;
+import com.bugkillers.service.artItem.UpdateArtItemService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,14 +14,35 @@ import org.springframework.web.bind.annotation.*;
 public class ArtItemController {
 
     private final SaveArtItemService saveArtItemService;
+    private final UpdateArtItemService updateArtItemService;
+    private final DeleteArtItemService deleteArtItemService;
+    private final GetArtItemService getArtItemService;
 
-    public ArtItemController(SaveArtItemService saveArtItemService) {
+    public ArtItemController(SaveArtItemService saveArtItemService, UpdateArtItemService updateArtItemService, DeleteArtItemService deleteArtItemService, GetArtItemService getArtItemService) {
         this.saveArtItemService = saveArtItemService;
+        this.updateArtItemService = updateArtItemService;
+        this.deleteArtItemService = deleteArtItemService;
+        this.getArtItemService = getArtItemService;
     }
 
     @PostMapping("/artItem")
-    public void saveArtItem (@RequestBody SaveArtItemDto saveArtItemDto) {
-        saveArtItemService.saveArtItem(saveArtItemDto);
+    public void save (@RequestBody SaveArtItemDto saveArtItemDto) {
+        saveArtItemService.save(saveArtItemDto);
+    }
+
+    @PutMapping("/artItem/{ano}")
+    public void update (@PathVariable Long ano, @RequestBody UpdateArtItemDto updateArtItemDto){
+        updateArtItemService.update(ano, updateArtItemDto);
+    }
+
+    @DeleteMapping("artItem/{ano}")
+    public void delete (@PathVariable Long ano){
+        deleteArtItemService.delete(ano);
+    }
+
+    @GetMapping("artItem/{ano}")
+    public GetArtItemDto findByAno (@PathVariable Long ano){
+        return getArtItemService.findByAno(ano);
     }
 
 
