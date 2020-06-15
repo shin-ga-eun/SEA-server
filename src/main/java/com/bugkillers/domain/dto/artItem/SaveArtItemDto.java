630a14f0d2c39c6a3e544b4c9a106eaefba64a99
shin-ga-eun <sge7102@naver.com>
2020-06-15 16:07:23 +0900

    #4 작품수정, 삭제, ano로 조회구현.

--- src/main/java/com/bugkillers/domain/dto/artItem/SaveArtItemDto.java
+++ src/main/java/com/bugkillers/domain/dto/artItem/SaveArtItemDto.java
@@ -1,14 +1,10 @@
 package com.bugkillers.domain.dto.artItem;
 
-import java.time.LocalDate;
-
 public class SaveArtItemDto {
     private String artist; //조인해야혀
     private String title;
     private String description;
     private int price;
-    private LocalDate create_at;
-    private LocalDate modify_at;
 
     public String getArtist() {
         return artist;
@@ -40,21 +36,5 @@ public class SaveArtItemDto {
 
     public void setPrice(int price) {
         this.price = price;
-    }
-
-    public LocalDate getCreate_at() {
-        return create_at;
-    }
-
-    public void setCreate_at(LocalDate create_at) {
-        this.create_at = create_at;
-    }
-
-    public LocalDate getModify_at() {
-        return modify_at;
-    }
-
-    public void setModify_at(LocalDate modify_at) {
-        this.modify_at = modify_at;
     }
 }
